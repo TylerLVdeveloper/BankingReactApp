ca044dae75982931dcd04fb1db9763e08b641df1
Tyler <tylerd062292@gmail.com>
2021-12-03 11:06:49 -0800

    checkpoint

--- src/AccountData.js
+++ src/AccountData.js
@@ -1,8 +1,9 @@
 const accountData = [
   {
     accountType: "Checking",
-    balance: 200.0,
-    key: 0,
+    accountNumber: "xxxxxx1234",
+    balance: 100.0,
+    key: "0",
     transactions: [
       { id: 0, amount: 50, type: "deposit" },
       { id: 1, amount: 500, type: "deposit" },
@@ -10,8 +11,21 @@ const accountData = [
   },
   {
     accountType: "Savings",
-    balance: 4320,
-    key: 1,
+    accountNumber: "xxxxxx5690",
+    balance: 100,
+    key: "1",
+    transactions: [
+      { id: 0, amount: 100, type: "deposit" },
+      { id: 1, amount: 500, type: "deposit" },
+      { id: 2, amount: 500, type: "deposit" },
+    ],
+  },
+
+  {
+    accountType: "Secondary Savings",
+    accountNumber: "xxxxxx5489",
+    balance: 100,
+    key: "2",
     transactions: [
       { id: 0, amount: 100, type: "deposit" },
       { id: 1, amount: 500, type: "deposit" },
--- src/Components/TransferFunds.js
+++ src/Components/TransferFunds.js
@@ -1,9 +1,116 @@
 import React from "react";
 import "../Stylesheets/TransferFunds.css";
+import accountData from "../AccountData";
+import dollarSign from "../images/dollarSign.png";
 
 class TransferFunds extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sendingAccnt: "0",
+      receivingAccnt: "1",
+      transferAmount: "",
+    };
+    this.updateReceivingAccntSelection =
+      this.updateReceivingAccntSelection.bind(this);
+    this.updateSendingAccntSelection =
+      this.updateSendingAccntSelection.bind(this);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  updateReceivingAccntSelection(e) {
+    this.setState({
+      receivingAccnt: e.target.value,
+    });
+  }
+
+  updateSendingAccntSelection(e) {
+    this.setState({
+      sendingAccnt: e.target.value,
+    });
+  }
+
+  handleChange(e) {
+    this.setState({ transferAmount: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let sendAccnt = accountData.find((account) => {
+      if (account.key === this.state.sendingAccnt) {
+        return Number(account.balance);
+      } else {
+        return null;
+      }
+    });
+    let recAccnt = accountData.find((account) => {
+      if (account.key === this.state.receivingAccnt) {
+        return Number(account.balance);
+      } else {
+        return null;
+      }
+    });
+    sendAccnt.balance -= this.state.transferAmount;
+    recAccnt.balance += Number(this.state.transferAmount);
+  }
+
   render() {
-    return <div className="coming_soon">Coming Soon</div>;
+    return (
+      <div id="transfer_container">
+        <h3>From</h3>
+        <select
+          id="From"
+          name="From"
+          onChange={(e) => this.updateSendingAccntSelection(e)}
+          className="transfer_field"
+        >
+          {accountData.map((account) => {
+            return (
+              <option value={account.key} key={account.key}>
+                {account.accountType} ({account.accountNumber})
+              </option>
+            );
+          })}
+        </select>
+
+        <h3>To</h3>
+        <select
+          id="From"
+          name="To"
+          onChange={(e) => this.updateReceivingAccntSelection(e)}
+          className="transfer_field"
+        >
+          {accountData.map((account) => {
+            if (account.key !== this.state.sendingAccnt) {
+              return (
+                <option value={account.key} key={account.key}>
+                  {account.accountType} ({account.accountNumber})
+                </option>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </select>
+
+        <img src={dollarSign} alt="" id="dollarSign" />
+        <div id="amount_field">
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="number"
+              id="Amount"
+              className="transfer_field"
+              placeholder="ex 20.00"
+              value={this.state.transferAmount}
+              onChange={this.handleChange}
+            />
+            <input type="submit" value="Transfer" id="transfer_btn" />
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
