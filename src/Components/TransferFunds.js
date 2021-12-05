1657bacd833bce10512f3c7592b13c3c8aec6cbb
Tyler <tylerd062292@gmail.com>
2021-12-05 11:43:16 -0800

    TransferFunds screen updates - Added confirmation screen, form completion validation

--- src/Components/TransferFunds.js
+++ src/Components/TransferFunds.js
@@ -3,13 +3,25 @@ import "../Stylesheets/TransferFunds.css";
 import accountData from "../AccountData";
 import dollarSign from "../images/dollarSign.png";
 
-class TransferFunds extends React.Component {
+class ConfirmationScreen extends React.Component {
+  render() {
+    return (
+      <div className="confirmation_container">
+        Confirm Transaction <br />
+        <div onClick={this.props.confirm}>Yes</div>
+      </div>
+    );
+  }
+}
+
+class TransferDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sendingAccnt: "0",
-      receivingAccnt: "1",
+      sendingAccnt: "",
+      receivingAccnt: "",
       transferAmount: "",
+      readyToSubmit: false,
     };
     this.updateReceivingAccntSelection =
       this.updateReceivingAccntSelection.bind(this);
@@ -18,22 +30,43 @@ class TransferFunds extends React.Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.checkFormCompletion = this.checkFormCompletion.bind(this);
+  }
+
+  checkFormCompletion() {
+    if (
+      this.state.sendingAccnt !== "" &&
+      this.state.receivingAccnt !== "" &&
+      this.state.transferAmount !== ""
+    ) {
+      this.setState({ readyToSubmit: true });
+    } else {
+      this.setState({ readyToSubmit: false });
+    }
   }
 
   updateReceivingAccntSelection(e) {
-    this.setState({
-      receivingAccnt: e.target.value,
-    });
+    this.setState(
+      {
+        receivingAccnt: e.target.value,
+      },
+      () => this.checkFormCompletion()
+    );
   }
 
   updateSendingAccntSelection(e) {
-    this.setState({
-      sendingAccnt: e.target.value,
-    });
+    this.setState(
+      {
+        sendingAccnt: e.target.value,
+      },
+      () => this.checkFormCompletion()
+    );
   }
 
   handleChange(e) {
-    this.setState({ transferAmount: e.target.value });
+    this.setState({ transferAmount: e.target.value }, () =>
+      this.checkFormCompletion()
+    );
   }
 
   handleSubmit(e) {
@@ -52,8 +85,14 @@ class TransferFunds extends React.Component {
         return null;
       }
     });
-    sendAccnt.balance -= this.state.transferAmount;
-    recAccnt.balance += Number(this.state.transferAmount);
+    let transferAmount = this.state.transferAmount;
+    let transactionDetails = {
+      sendAccnt,
+      recAccnt,
+      transferAmount,
+    };
+
+    this.props.startTransaction(transactionDetails);
   }
 
   render() {
@@ -66,6 +105,9 @@ class TransferFunds extends React.Component {
           onChange={(e) => this.updateSendingAccntSelection(e)}
           className="transfer_field"
         >
+          <option value="" disabled selected>
+            Select an account
+          </option>
           {accountData.map((account) => {
             return (
               <option value={account.key} key={account.key}>
@@ -82,6 +124,9 @@ class TransferFunds extends React.Component {
           onChange={(e) => this.updateReceivingAccntSelection(e)}
           className="transfer_field"
         >
+          <option value="" disabled selected>
+            Select an account
+          </option>
           {accountData.map((account) => {
             if (account.key !== this.state.sendingAccnt) {
               return (
@@ -106,11 +151,54 @@ class TransferFunds extends React.Component {
               value={this.state.transferAmount}
               onChange={this.handleChange}
             />
-            <input type="submit" value="Transfer" id="transfer_btn" />
+            <input
+              type={this.state.readyToSubmit ? "submit" : "hidden"}
+              value="Transfer"
+              id="transfer_btn"
+            />
           </form>
         </div>
       </div>
     );
+  }
+}
+
+class TransferFunds extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      screenView: "transferDetails",
+    };
+    this.startTransaction = this.startTransaction.bind(this);
+    this.processTransaction = this.processTransaction.bind(this);
+  }
+
+  startTransaction(transactionDetails) {
+    this.setState({ screenView: "confirmationScreen" });
+
+    this.setState({
+      currentTransaction: transactionDetails,
+    });
+  }
+
+  processTransaction() {
+    let transaction = this.state.currentTransaction;
+    transaction.sendAccnt.balance -= Number(transaction.transferAmount);
+    transaction.recAccnt.balance += Number(transaction.transferAmount);
+  }
+
+  render() {
+    if (this.state.screenView === "transferDetails")
+      return (
+        <TransferDetails
+          startTransaction={(transactionDetails) =>
+            this.startTransaction(transactionDetails)
+          }
+        />
+      );
+
+    if (this.state.screenView === "confirmationScreen")
+      return <ConfirmationScreen confirm={() => this.processTransaction()} />;
   }
 }
 
