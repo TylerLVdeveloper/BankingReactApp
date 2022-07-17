80089cdad27d8e36804f6262470e02d6b4c43ec4
Tyler Dickinson <tylerd062292@gmail.com>
2022-07-16 18:30:30 -0700

    Transaction Detail updates

--- src/Components/TransactionDetailsModal.js
+++ src/Components/TransactionDetailsModal.js
@@ -7,8 +7,25 @@ import cancelIcon from "../images/cancelIcon.png";
 class TransactionDetailsModal extends React.Component {
   render() {
     return (
-      <div className="confirmation_modal_overlay" ref={this.props.nodeRef}>
-        <div id="confirmation_modal">test</div>
+      <div className="trxn_details_modal_overlay" ref={this.props.nodeRef}>
+        <div id="trxn_details_modal">
+          <div id="trxn_type">
+            {this.props.trxn.type}
+            <div id="trxn_origin">{this.props.trxn.origin}</div>
+          </div>
+
+          <div id="trxn_amount">
+            ${this.props.trxn.amount}
+            <div id="trxn_balance">
+              Ending Balance: ${this.props.trxn.endingBalance}
+            </div>
+          </div>
+
+          <div id="trxn_date">
+            <div id="trxn_time">{this.props.trxn.time}</div>
+            {this.props.trxn.date}
+          </div>
+        </div>
       </div>
     );
   }
